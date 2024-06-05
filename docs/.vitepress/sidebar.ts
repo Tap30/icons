0d2b51cc4b8ccc0d2c2bf68e6380c291f12ecc80
Amirhossein Alibakhshi <amir78729@gmail.com>
2024-06-05 21:22:03 +0330

    fix: change sidebar

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -5,28 +5,20 @@ type Content = { name: string; title: string }
 
 const getCategories = (): DefaultTheme.SidebarItem[] => {
   const categories = JSON.parse(fs.readFileSync('docs/.vitepress/data/categories.json').toString()) as { name: string, items: string[] }[];
-  return categories.map((category) => ({ link: `/category/${category.name}`, text: category.name }))
-}
-
-const getPackages = (): DefaultTheme.SidebarItem[] => {
-  const packages = JSON.parse(fs.readFileSync('docs/.vitepress/data/packages.json').toString()) as Content[];
-  return packages.map((pck) => ({ link: `/package/${pck.name}`, text: pck.title }))
+  return [
+    {
+      text: 'All Icons',
+      link: '/all',
+    },
+    ...categories.map((category) => ({ link: `/category/${category.name}`, text: category.name }))
+  ]
 }
 
 export default [
-  // { text: 'Getting Started', link: '/getting-started' },
-  {
-    text: 'All',
-    link: '/all',
-  },
+  { text: 'Getting Started', link: '/getting-started' },
   {
     text: 'Icon Categories',
     collapsed: false,
     items: getCategories(),
-  },
-  {
-    text: 'Packages',
-    collapsed: false,
-    items: getPackages(),
   },
 ] as DefaultTheme.SidebarItem[]
